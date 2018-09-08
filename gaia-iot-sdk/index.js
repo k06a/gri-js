b9c7890b602039071c91efd91fbec735993be253
Pong Cheecharern <pongcheecharern@macbook-pro-5.lan>
2018-09-08 23:45:18 +0700

    added content to the iot script

--- gaia-iot-sdk/index.js
+++ gaia-iot-sdk/index.js
@@ -1,12 +1,28 @@
-var rpiDhtSensor = require('rpi-dht-sensor');
- 
-var dht = new rpiDhtSensor.DHT22(2);
- 
-function read () {
-  var readout = dht.read();
- 
-    console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
-        'humidity: ' + readout.humidity.toFixed(2) + '%');
-    setTimeout(read, 5000);
+const rpiDhtSensor = require('rpi-dht-sensor');
+const axios = require('axios') 
+const dht = new rpiDhtSensor.DHT22(2);
+const gaiaNode = "192.168.86.97" 
+function submitData () {
+  const readout = dht.read();
+  let _data = {
+      temperature: readout.temperature.toFixed(10)
+  }
+  let _meta = {
+        coordinates: "kx-building",
+        deviceType: "temperature_sensor",
+        date: Date.now()
+  }
+  axios.post('http://192.168.86.97:3000/submitData', {
+    data: data,
+    meta: _meta
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+    
+    setTimeout(submitData, 3000);
 }
-read();
+submitData();
